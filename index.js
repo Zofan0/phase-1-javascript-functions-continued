8413821933586d071597066a0891922ea989cc37
Zofan0 <zpshark@gmail.com>
2024-11-21 15:11:27 +1000

    done module

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // code your solution here
+function saturdayFun(n) {
+    var a = '';
+    if (n) {
+       a = n; 
+    } else {
+        a = 'roller-skate';
+    }
+    return 'This Saturday, I want to ' + a + '!';
+}
+
+function mondayWork(n) {
+    var a = ''
+    if (n) {
+        a = n; 
+     } else {
+        a = 'go to the office';
+     }
+     return 'This Monday, I will ' + a + '.';
+}
+
+function wrapAdjective(n) {
+    var a = ''
+    if (n) {
+        a = n; 
+     } else {
+         a = 'special';
+     }
+     return function (abc) {
+        var b = ''
+        if (abc) {
+            b = abc; 
+            } else {
+                b = '*';
+            }
+        return "You are " + n +b + n+"!"; 
+     }
+}
